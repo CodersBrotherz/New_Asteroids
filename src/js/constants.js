55908d6262b51f90b3520881193a749c9697041e
Pere <pcerqueda@julia.ad>
2025-02-28 14:00:58 +0100

    Ajustar constantes de asteroides y dimensiones de pantalla

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,14 +1,14 @@
 export const AST_DEF_RADIUS = 70
 export const AST_SPEED = 2
-export const AST_MIN_SIZE = 15
-export const AST_COUNT = 8
-export const BULLET_RANGE = 1500 // Time in ms
+export const AST_MIN_SIZE = 20
+export const AST_COUNT = 5
+export const BULLET_RANGE = 1000 // Time in ms
 export const SHIP_SPEED = 6
 export const SCREEN_MOBILE = {
-  width: 1080,
-  height: 1920
+  width: 900,
+  height: 1600
 }
 export const SCREEN_DEFAULT = {
-  width: 1920,
-  height: 1080
+  width: 1600,
+  height: 900
 }
